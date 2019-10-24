05060fe8e6391af34205d61ecdc3fc1c41080159
sandeep reddy <sandeepreddyg77@outlook.com>
2019-10-24 11:00:00 +0530

    q

---
+++ Shipping/App_Start/BundleConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace Shipping.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bower_components/bootstrap/dist/css/bootstrap.min.css",
+                "~/Content/bower_components/font-awesome/css/font-awesome.min.css",
+                "~/Content/bower_components/Ionicons/css/ionicons.min.css",
+                  "~/Content/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css",
+                "~/Content/bower_components/bootstrap-daterangepicker/daterangepicker.css",
+                  "~/Content/dist/css/AdminLTE.min.css",
+                "~/Content/dist/css/skins/_all-skins.min.css",
+                  "~/Content/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css",
+                "~/Content/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css"
+               ));
+
+        }
+    }
+}
